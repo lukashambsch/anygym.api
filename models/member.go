a7e143dbc49ec79f1f957c4e39e3b8e529e52fa5
Lukas Hambsch <lukas.hambsch@gmail.com>
2016-08-17 11:42:34 -0700

    Made FKs in member struct nullable.

--- models/member.go
+++ models/member.go
@@ -2,9 +2,9 @@ package models
 
 type Member struct {
 	MemberId  int64  `json:"member_id"`
-	UserId    int64  `json:"user_id"`
-	AddressId int64  `json:"address_id"`
-	PlanId    int64  `json:"plan_id"`
+	UserId    *int64 `json:"user_id"`
+	AddressId *int64 `json:"address_id"`
+	PlanId    *int64 `json:"plan_id"`
 	Active    bool   `json:"active"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`

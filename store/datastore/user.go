b810fc2b374bb738d8bbeb44e939e5682716bad3
Lukas Hambsch <lukas.hambsch@gmail.com>
2016-08-18 09:09:26 -0700

    Added user datastore.

---
+++ store/datastore/user.go
@@ -0,0 +1,175 @@
+package datastore
+
+import (
+	"github.com/lukashambsch/gym-all-over/models"
+	"github.com/lukashambsch/gym-all-over/store"
+)
+
+func GetUserList() ([]models.User, error) {
+	var (
+		users []models.User
+		user  models.User
+	)
+
+	rows, err := store.DB.Query(getUserListQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		err = rows.Scan(
+			&user.UserId,
+			&user.Email,
+			&user.Token,
+			&user.Secret,
+			&user.PasswordSalt,
+			&user.PasswordHash,
+			&user.CreatedOn,
+		)
+		users = append(users, user)
+		if err != nil {
+			return nil, err
+		}
+	}
+	defer rows.Close()
+
+	return users, nil
+}
+
+func GetUserCount() (*int, error) {
+	var count int
+
+	row := store.DB.QueryRow(getUserCountQuery)
+	err := row.Scan(&count)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &count, nil
+}
+
+func GetUser(userId int64) (*models.User, error) {
+	var user models.User
+
+	row := store.DB.QueryRow(getUserQuery, userId)
+	err := row.Scan(
+		&user.UserId,
+		&user.Email,
+		&user.Token,
+		&user.Secret,
+		&user.PasswordSalt,
+		&user.PasswordHash,
+		&user.CreatedOn,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func CreateUser(user models.User) (*models.User, error) {
+	var created models.User
+
+	row := store.DB.QueryRow(
+		createUserQuery,
+		user.Email,
+		user.Token,
+		user.Secret,
+		user.PasswordSalt,
+		user.PasswordHash,
+	)
+	err := row.Scan(
+		&created.UserId,
+		&created.Email,
+		&created.Token,
+		&created.Secret,
+		&created.PasswordSalt,
+		&created.PasswordHash,
+		&created.CreatedOn,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &created, nil
+}
+
+func UpdateUser(userId int64, user models.User) (*models.User, error) {
+	var updated models.User
+
+	row := store.DB.QueryRow(
+		updateUserQuery,
+		user.Email,
+		user.Token,
+		user.Secret,
+		user.PasswordSalt,
+		user.PasswordHash,
+		userId,
+	)
+	err := row.Scan(
+		&updated.UserId,
+		&updated.Email,
+		&updated.Token,
+		&updated.Secret,
+		&updated.PasswordSalt,
+		&updated.PasswordHash,
+		&updated.CreatedOn,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &updated, nil
+}
+
+func DeleteUser(userId int64) error {
+	stmt, err := store.DB.Prepare(deleteUserQuery)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(userId)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+const getUserListQuery = `
+SELECT *
+FROM users
+`
+
+const getUserQuery = `
+SELECT *
+FROM users
+WHERE user_id = $1
+`
+
+const createUserQuery = `
+INSERT INTO users (email, token, secret, password_salt, password_hash)
+VALUES ($1, $2, $3, $4, $5)
+RETURNING user_id, email, token, secret, password_salt, password_hash, created_on
+`
+
+const updateUserQuery = `
+UPDATE users
+SET email = $1, token = $2, secret = $3, password_salt = $4, password_hash = $5
+WHERE user_id = $6
+RETURNING user_id, email, token, secret, password_salt, password_hash, created_on
+`
+
+const deleteUserQuery = `
+DELETE
+FROM users
+WHERE user_id = $1
+`
+
+const getUserCountQuery = `
+SELECT count(*)
+FROM users
+`
---
+++ store/datastore/user_test.go
@@ -0,0 +1,157 @@
+package datastore_test
+
+import (
+	"github.com/lukashambsch/gym-all-over/models"
+	"github.com/lukashambsch/gym-all-over/store/datastore"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("User db interactions", func() {
+	var userId int64 = 1
+
+	Describe("GetUserList", func() {
+		var users []models.User
+
+		Describe("Successful call", func() {
+			BeforeEach(func() {
+				users, _ = datastore.GetUserList()
+			})
+
+			It("should return a list of users", func() {
+				Expect(len(users)).To(Equal(2))
+			})
+		})
+	})
+
+	Describe("GetUser", func() {
+		var user *models.User
+
+		Describe("Successful call", func() {
+			It("should return the correct user", func() {
+				user, _ = datastore.GetUser(userId)
+				Expect(user.UserId).To(Equal(userId))
+			})
+		})
+
+		Describe("Unsuccessful call", func() {
+			var (
+				nonExistentId int64 = 5
+				err           error
+			)
+
+			BeforeEach(func() {
+				user, err = datastore.GetUser(nonExistentId)
+			})
+
+			It("should return an error", func() {
+				Expect(err).ToNot(BeNil())
+			})
+
+			It("should return a nil user", func() {
+				Expect(user).To(BeNil())
+			})
+		})
+	})
+
+	Describe("GetUserCount", func() {
+		var count *int
+
+		Describe("Successful call", func() {
+			BeforeEach(func() {
+				count, _ = datastore.GetUserCount()
+			})
+
+			It("should return the correct count", func() {
+				Expect(*count).To(Equal(2))
+			})
+		})
+	})
+
+	Describe("CreateUser", func() {
+		var (
+			email   string = "test@gmail.com"
+			user    models.User
+			created *models.User
+		)
+
+		Describe("Successful call", func() {
+			BeforeEach(func() {
+				user = models.User{Email: email}
+				created, _ = datastore.CreateUser(user)
+			})
+
+			AfterEach(func() {
+				datastore.DeleteUser(created.UserId)
+			})
+
+			It("should return the created user", func() {
+				Expect(created.Email).To(Equal(email))
+			})
+
+			It("should add a user to the db", func() {
+				newMember, _ := datastore.GetUser(created.UserId)
+				Expect(newMember.Email).To(Equal(email))
+			})
+		})
+
+		Describe("Unsuccessful call", func() {
+			It("should return an error object if no email", func() {
+				mbr := models.User{}
+				_, err := datastore.CreateUser(mbr)
+				Expect(err).ToNot(BeNil())
+			})
+		})
+	})
+
+	Describe("UpdateUser", func() {
+		var (
+			email   string = "test@gmail.com"
+			user    models.User
+			created *models.User
+			updated *models.User
+			err     error
+		)
+
+		Describe("Successful call", func() {
+			BeforeEach(func() {
+				created, _ = datastore.CreateUser(models.User{Email: "different"})
+				created.Email = email
+				updated, _ = datastore.UpdateUser(created.UserId, *created)
+			})
+
+			AfterEach(func() {
+				datastore.DeleteUser(updated.UserId)
+			})
+
+			It("should return the updated user", func() {
+				Expect(updated.Email).To(Equal(email))
+			})
+		})
+
+		Describe("Unsuccessful call", func() {
+			BeforeEach(func() {
+				user = models.User{Email: email}
+				updated, err = datastore.UpdateUser(3, user)
+			})
+
+			It("should return an error object if user to update doesn't exist", func() {
+				Expect(err).ToNot(BeNil())
+			})
+
+			It("should return a nil user", func() {
+				Expect(updated).To(BeNil())
+			})
+		})
+	})
+
+	Describe("DeleteUser", func() {
+		Describe("Successful call", func() {
+			It("should return nil", func() {
+				created, _ := datastore.CreateUser(models.User{Email: "email"})
+				err := datastore.DeleteUser(created.UserId)
+				Expect(err).To(BeNil())
+			})
+		})
+	})
+})

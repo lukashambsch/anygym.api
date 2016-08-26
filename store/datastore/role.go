3c439a0e6d1921954beb2d4ef795bec726275586
Lukas Hambsch <lukas.hambsch@gmail.com>
2016-08-26 06:59:35 -0700

    Added role datastore.

---
+++ store/datastore/role.go
@@ -0,0 +1,129 @@
+package datastore
+
+import (
+	"fmt"
+
+	"github.com/lukashambsch/gym-all-over/models"
+	"github.com/lukashambsch/gym-all-over/store"
+)
+
+func GetRoleList(where string) ([]models.Role, error) {
+	var (
+		roles []models.Role
+		role  models.Role
+	)
+
+	query := fmt.Sprintf("%s %s", getRoleListQuery, where)
+	rows, err := store.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		err = rows.Scan(&role.RoleId, &role.RoleName)
+		roles = append(roles, role)
+		if err != nil {
+			return nil, err
+		}
+	}
+	defer rows.Close()
+
+	return roles, nil
+}
+
+func GetRoleCount() (*int, error) {
+	var count int
+
+	row := store.DB.QueryRow(getRoleCountQuery)
+	err := row.Scan(&count)
+	if err != nil {
+		return nil, err
+	}
+
+	return &count, nil
+}
+
+func GetRole(roleId int64) (*models.Role, error) {
+	var role models.Role
+
+	row := store.DB.QueryRow(getRoleQuery, roleId)
+	err := row.Scan(&role.RoleId, &role.RoleName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &role, nil
+}
+
+func CreateRole(role models.Role) (*models.Role, error) {
+	var created models.Role
+
+	row := store.DB.QueryRow(createRoleQuery, role.RoleName)
+	err := row.Scan(&created.RoleId, &created.RoleName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &created, nil
+}
+
+func UpdateRole(roleId int64, role models.Role) (*models.Role, error) {
+	var updated models.Role
+
+	row := store.DB.QueryRow(updateRoleQuery, role.RoleName, roleId)
+	err := row.Scan(&updated.RoleId, &updated.RoleName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &updated, nil
+}
+
+func DeleteRole(roleId int64) error {
+	stmt, err := store.DB.Prepare(deleteRoleQuery)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(roleId)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+const getRoleListQuery = `
+SELECT *
+FROM roles
+`
+
+const getRoleQuery = `
+SELECT *
+FROM roles
+WHERE role_id = $1
+`
+
+const createRoleQuery = `
+INSERT INTO roles (role_name)
+VALUES ($1)
+RETURNING role_id, role_name
+`
+
+const updateRoleQuery = `
+UPDATE roles
+SET role_name = $1
+WHERE role_id = $2
+RETURNING role_id, role_name
+`
+
+const deleteRoleQuery = `
+DELETE
+FROM roles
+WHERE role_id = $1
+`
+
+const getRoleCountQuery = `
+SELECT count(*)
+FROM roles
+`
---
+++ store/datastore/role_test.go
@@ -0,0 +1,159 @@
+package datastore_test
+
+import (
+	"github.com/lukashambsch/gym-all-over/models"
+	"github.com/lukashambsch/gym-all-over/store/datastore"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Role db interactions", func() {
+	var roleId int64 = 1
+
+	Describe("GetRoleList", func() {
+		var roles []models.Role
+
+		Describe("Successful call", func() {
+			BeforeEach(func() {
+				roles, _ = datastore.GetRoleList("")
+			})
+
+			It("should return a list of roles", func() {
+				Expect(len(roles)).To(Equal(5))
+			})
+		})
+	})
+
+	Describe("GetRole", func() {
+		var role *models.Role
+
+		Describe("Successful call", func() {
+			It("should return the correct role", func() {
+				role, _ = datastore.GetRole(roleId)
+				Expect(role.RoleId).To(Equal(roleId))
+			})
+		})
+
+		Describe("Unsuccessful call", func() {
+			var (
+				nonExistentId int64 = 5000
+				err           error
+			)
+
+			BeforeEach(func() {
+				role, err = datastore.GetRole(nonExistentId)
+			})
+
+			It("should return an error", func() {
+				Expect(err).ToNot(BeNil())
+			})
+
+			It("should return a nil role", func() {
+				Expect(role).To(BeNil())
+			})
+		})
+	})
+
+	Describe("GetRoleCount", func() {
+		var count *int
+
+		Describe("Successful call", func() {
+			BeforeEach(func() {
+				count, _ = datastore.GetRoleCount()
+			})
+
+			It("should return the correct count", func() {
+				Expect(*count).To(Equal(5))
+			})
+		})
+	})
+
+	Describe("CreateRole", func() {
+		var (
+			roleName string = "New Role"
+			role     models.Role
+			created  *models.Role
+		)
+
+		Describe("Successful call", func() {
+			BeforeEach(func() {
+				role = models.Role{RoleName: roleName}
+				created, _ = datastore.CreateRole(role)
+			})
+
+			AfterEach(func() {
+				datastore.DeleteRole(created.RoleId)
+			})
+
+			It("should return the created role", func() {
+				Expect(created.RoleName).To(Equal(roleName))
+			})
+
+			It("should add a role to the db", func() {
+				newRole, _ := datastore.GetRole(created.RoleId)
+				Expect(newRole.RoleName).To(Equal(roleName))
+			})
+		})
+
+		Describe("Unsuccessful call", func() {
+			It("should return an error object if role is not unique", func() {
+				name := "Test Name"
+				pln := models.Role{RoleName: name}
+				datastore.CreateRole(pln)
+				_, err := datastore.CreateRole(pln)
+				Expect(err).ToNot(BeNil())
+			})
+		})
+	})
+
+	Describe("UpdateRole", func() {
+		var (
+			roleName string = "Anytime"
+			role     models.Role
+			created  *models.Role
+			updated  *models.Role
+			err      error
+		)
+
+		Describe("Successful call", func() {
+			BeforeEach(func() {
+				role = models.Role{RoleName: roleName}
+				created, _ = datastore.CreateRole(models.Role{RoleName: "Daily"})
+				updated, _ = datastore.UpdateRole(created.RoleId, role)
+			})
+
+			AfterEach(func() {
+				datastore.DeleteRole(updated.RoleId)
+			})
+
+			It("should return the updated role", func() {
+				Expect(updated.RoleName).To(Equal(roleName))
+			})
+		})
+
+		Describe("Unsuccessful call", func() {
+			BeforeEach(func() {
+				role = models.Role{RoleName: "Daily"}
+				updated, err = datastore.UpdateRole(10000, role)
+			})
+
+			It("should return an error object if role to update doesn't exist", func() {
+				Expect(err).ToNot(BeNil())
+			})
+
+			It("should return a nil role", func() {
+				Expect(updated).To(BeNil())
+			})
+		})
+	})
+
+	Describe("DeleteRole", func() {
+		Describe("Successful call", func() {
+			It("should return nil", func() {
+				created, _ := datastore.CreateRole(models.Role{RoleName: "Testing"})
+				err := datastore.DeleteRole(created.RoleId)
+				Expect(err).To(BeNil())
+			})
+		})
+	})
+})

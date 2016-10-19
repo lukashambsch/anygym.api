e768f31b132575b37fd0be5fe31061aae62eff26
Lukas Hambsch <lukas.hambsch@gmail.com>
2016-10-19 09:22:51 -0700

    Updated test.sh w/ Docker env variables.

--- test.sh
+++ test.sh
@@ -3,12 +3,12 @@ export POSTGRES_ENV_POSTGRES_PASSWORD=pa55word
 export POSTGRES_PORT_5432_TCP_ADDR=localhost
 export POSTGRES_PORT_5432_TCP_PORT=5432
 export DATABASE_DRIVER="postgres"
-export DATABASE_CONFIG="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/postgres?sslmode=disable"
+export DATABASE_CONFIG="postgres://$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@localhost:5432/postgres?sslmode=disable"
 go get -t
 go get github.com/onsi/ginkgo
 go get github.com/onsi/gomega
 go get github.com/mattes/migrate
-psql -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
+psql -c "CREATE USER $POSTGRES_ENV_POSTGRES_USER WITH PASSWORD '$POSTGRES_ENV_POSTGRES_PASSWORD';"
 echo "CREATE EXTENSION IF NOT EXISTS pgcrypto" | psql -d postgres
 migrate -url $DATABASE_CONFIG -path ./store/migrations up
 go test ./... -cover -v

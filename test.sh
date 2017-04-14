a1ce086376c242d727caae4e9cf555e2d88b5aa6
Lukas Hambsch <lukas.hambsch@gmail.com>
2017-04-13 17:46:52 -0700

    Add package versioning w/ dep.

--- test.sh
+++ test.sh
@@ -6,13 +6,9 @@ export POSTGRES_PORT_5432_TCP_ADDR=localhost
 export POSTGRES_PORT_5432_TCP_PORT=5432
 export DATABASE_DRIVER="postgres"
 export DATABASE_CONFIG="postgres://$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@localhost:5432/postgres?sslmode=disable"
-go get -t
-go get github.com/onsi/ginkgo
-go get github.com/onsi/gomega
-#go get github.com/mattes/migrate
 psql -c "CREATE USER $POSTGRES_ENV_POSTGRES_USER WITH PASSWORD '$POSTGRES_ENV_POSTGRES_PASSWORD';"
 echo "CREATE EXTENSION IF NOT EXISTS pgcrypto" | psql -d postgres
 migrate -url $DATABASE_CONFIG -path ./store/migrations up
-go test ./... -cover -v
+go test -cover -v $(go list ./... | grep -v /vendor/)
 migrate -url $DATABASE_CONFIG -path ./store/migrations down
 export GOENV=local

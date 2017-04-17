e6d29a18a9b08038aa8bf73955db9745d6fb5028
Lukas Hambsch <lukas.hambsch@gmail.com>
2017-04-17 14:53:29 -0700

    Change to use glide until dep is more stable.

--- store/store.go
+++ store/store.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-    "os"
     "time"
 
 	_ "github.com/lib/pq"
--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 # shell script to set up env and run tests
 export GOENV=test
-go get -u github.com/golang/dep/...
+
+# go get -u github.com/golang/dep/...
+if ! type "glide" > /dev/null; then
+  curl https://glide.sh/get | sh
+fi
+
 psql -c "CREATE USER $POSTGRES_ENV_POSTGRES_USER WITH PASSWORD '$POSTGRES_ENV_POSTGRES_PASSWORD';"
 echo "CREATE EXTENSION IF NOT EXISTS pgcrypto" | psql -d postgres
+glide install
 go run store/migrations/migrate.go -up
 go test -cover -v $(go list ./... | grep -v /vendor/)
 go run store/migrations/migrate.go -down
